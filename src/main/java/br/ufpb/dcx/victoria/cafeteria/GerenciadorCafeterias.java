52a409b4db0430da3457a833fe3249427fb55fee
Victória <santuusviih@gmail.com>
2026-05-27 18:33:47 -0300

    Classe GerenciadorCafeterias pronta e término do passo 1.

--- src/main/java/br/ufpb/dcx/victoria/cafeteria/GerenciadorCafeterias.java
+++ src/main/java/br/ufpb/dcx/victoria/cafeteria/GerenciadorCafeterias.java
@@ -1,4 +1,25 @@
 package br.ufpb.dcx.victoria.cafeteria;
+import br.ufpb.dcx.victoria.cafeteria.Cafeteria;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GerenciadorCafeterias {
+    private List<Cafeteria> listaCafeterias;
+
+    public GerenciadorCafeterias()
+    {
+        this.listaCafeterias = new ArrayList<>();
+    }
+    public void adicionarCafeterias(Cafeteria c) {
+        this.listaCafeterias.add(c);
+    }
+    public Cafeteria buscarCafeteriaPorNome(String nomeProcurado) throws Exception {
+        for (Cafeteria c : this.listaCafeterias) {
+            if (c.getNome().equalsIgnoreCase(nomeProcurado)) {
+                return c;
+            }
+        }
+        throw new Exception("Cafeteris não encontrada: " + nomeProcurado);
+    }
 }
